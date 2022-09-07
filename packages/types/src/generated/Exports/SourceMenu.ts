d0f2c863a313def74b2dbf873fd165f8f91d4c9f
Faizan Durrani <faizandurrani324@gmail.com>
2022-09-08 02:33:45 +0500

    resolve issues

--- packages/types/src/generated/Exports/SourceMenu.ts
+++
@@ -1,7 +0,0 @@
-import { SourceMenuItem } from "./../_exports";
-export interface SourceMenu {
-    items: SourceMenuItem[];
-}
-declare global {
-    function createSourceMenu(info: SourceMenu): SourceMenu;
-}
--- packages/types/src/generated/Exports/UserForm.ts
+++
@@ -1,7 +0,0 @@
-import { FormObject } from "./../_exports";
-export interface UserForm {
-    formElements: FormObject[];
-}
-declare global {
-    function createUserForm(info: UserForm): UserForm;
-}
