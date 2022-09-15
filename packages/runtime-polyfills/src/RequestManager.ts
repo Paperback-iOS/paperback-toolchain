c4a29a0eeb0941db3156fa05bfa1a0eb68959d82
Faizan Durrani <faizandurrani324@gmail.com>
2022-09-15 17:57:36 +0500

    cleanup

--- packages/runtime-polyfills/src/RequestManager.ts
+++ packages/runtime-polyfills/src/RequestManager.ts
@@ -1,78 +1,87 @@
 import { RequestManager, Request, Response, SourceInterceptor } from "@paperback/types"
 import { PaperbackPolyfills } from "./PaperbackPolyfills"
-
-//@ts-ignore
 import axios, { Method } from 'axios'
 
-PaperbackPolyfills.createRequestManager = function (info: {
-    interceptor?: SourceInterceptor
-    requestsPerSecond?: number
-    requestTimeout?: number
-}): RequestManager {
-    return {
-        interceptor: info.interceptor,
-        requestsPerSecond: info.requestsPerSecond ?? 2.5,
-        requestTimeout: info.requestTimeout ?? 20_000,
+class MockRequestManager implements RequestManager {
+    requestTimeout: number
+    requestsPerSecond: number
 
-        async getDefaultUserAgent() {
-            return ''
-        },
-        async schedule(request: Request, retryCount: number) {
-
-            // Pass this request through the interceptor if one exists
-            if (info.interceptor) {
-                request = await info.interceptor.interceptRequest(request)
-            }
-
-            // Append any cookies into the header properly
-            let headers: any = request.headers ?? {}
-
-            let cookieData = ''
-            for (let cookie of request.cookies ?? [])
-                cookieData += `${cookie.name}=${cookie.value};`
-
-            headers['cookie'] = cookieData
-
-            // If no user agent has been supplied, default to a basic Paperback-iOS agent
-            headers['user-agent'] = headers["user-agent"] ?? 'Paperback-iOS'
-
-            // If we are using a urlencoded form data as a post body, we need to decode the request for Axios
-            let decodedData = request.data
-            if (typeof decodedData == 'object') {
-                if (headers['content-type']?.includes('application/x-www-form-urlencoded')) {
-                    decodedData = ""
-                    Object.keys(request.data).forEach(attribute => {
-                        if (decodedData.length > 0) {
-                            decodedData += "&"
-                        }
-                        decodedData += `${attribute}=${request.data[attribute]}`
-                    })
-                }
-            }
-
-            // We must first get the response object from Axios, and then transcribe it into our own Response type before returning
-            let response = await axios(`${request.url}${request.param ?? ''}`, {
-                method: <Method>request.method,
-                headers: headers,
-                data: decodedData,
-                timeout: info.requestTimeout || 0,
-                responseType: 'arraybuffer'
-            })
-
-            let responsePacked: Response = {
-                rawData: App.createRawData(response.data),
-                data: Buffer.from(response.data, 'binary').toString(),
-                status: response.status,
-                headers: response.headers,
-                request: request
-            }
-
-            // Pass this through the response interceptor if one exists
-            if (info.interceptor) {
-                responsePacked = await info.interceptor.interceptResponse(responsePacked)
-            }
-
-            return responsePacked
-        }
+    constructor(
+        public interceptor?: SourceInterceptor,
+        requestsPerSecond?: number,
+        requestTimeout?: number
+    ) {
+        this.requestTimeout = requestTimeout ?? 20_000
+        this.requestsPerSecond = requestsPerSecond ?? 2.5
     }
+
+    async getDefaultUserAgent() {
+        return ''
+    }
+
+    async schedule(request: Request, retryCount: number) {
+
+        // Pass this request through the interceptor if one exists
+        if (this.interceptor) {
+            request = await this.interceptor.interceptRequest(request)
+        }
+
+        // Append any cookies into the header properly
+        let headers: any = request.headers ?? {}
+
+        let cookieData = ''
+        for (let cookie of request.cookies ?? [])
+            cookieData += `${cookie.name}=${cookie.value};`
+
+        headers['cookie'] = cookieData
+
+        // If no user agent has been supplied, default to a basic Paperback-iOS agent
+        headers['user-agent'] = headers["user-agent"] ?? 'Paperback-iOS'
+
+        // If we are using a urlencoded form data as a post body, we need to decode the request for Axios
+        let decodedData = request.data
+        if (typeof decodedData == 'object') {
+            if (headers['content-type']?.includes('application/x-www-form-urlencoded')) {
+                decodedData = ""
+                Object.keys(request.data).forEach(attribute => {
+                    if (decodedData.length > 0) {
+                        decodedData += "&"
+                    }
+                    decodedData += `${attribute}=${request.data[attribute]}`
+                })
+            }
+        }
+
+        // We must first get the response object from Axios, and then transcribe it into our own Response type before returning
+        let response = await axios(`${request.url}${request.param ?? ''}`, {
+            method: <Method>request.method,
+            headers: headers,
+            data: decodedData,
+            timeout: this.requestTimeout || 0,
+            responseType: 'arraybuffer'
+        })
+
+        let responsePacked: Response = {
+            rawData: App.createRawData(response.data),
+            data: Buffer.from(response.data, 'binary').toString(),
+            status: response.status,
+            headers: response.headers,
+            request: request
+        }
+
+        // Pass this through the response interceptor if one exists
+        if (this.interceptor) {
+            responsePacked = await this.interceptor.interceptResponse(responsePacked)
+        }
+
+        return responsePacked
+    }
+}
+
+PaperbackPolyfills.createRequestManager = function (info): RequestManager {
+    return new MockRequestManager(
+        info.interceptor,
+        info.requestsPerSecond,
+        info.requestTimeout
+    )
 }
--- packages/runtime-polyfills/src/SourceStateManager.ts
+++ packages/runtime-polyfills/src/SourceStateManager.ts
@@ -1,28 +1,19 @@
-import { SourceStateManager } from "@paperback/types"
+import { SecureStateManager, SourceStateManager } from "@paperback/types"
 import { PaperbackPolyfills } from "./PaperbackPolyfills"
 
-PaperbackPolyfills.createSourceStateManager = function (): SourceStateManager {
-    return {
-        store: function (key: string, value: unknown) {
-            // Fill this in so the test classes don't commit sudoku
-            virtualStateStore[key] = value
-            return Promise.resolve()
-        },
-        retrieve: function (key: string) {
-            return Promise.resolve(virtualStateStore[key])
-        },
-        keychain: {
-            store: function (key: string, value: unknown) {
-                // Fill this in so the test classes don't commit sudoku
-                virtualKeychainStore[key] = value
-                return Promise.resolve()
-            },
-            retrieve: function (key: string) {
-                return Promise.resolve(virtualKeychainStore[key])
-            }
-        }
+class MockSourceStateManager implements SourceStateManager {
+    readonly keychain: SecureStateManager = new MockSourceStateManager()
+
+    private objectStore: Record<any, any> = {}
+
+    store(key: any, value: any) {
+        this.objectStore[key] = value
+    }
+    retrieve(key: any) {
+        return this.objectStore[key]
     }
 }
 
-var virtualStateStore: any = {}
-var virtualKeychainStore: any = {}
+PaperbackPolyfills.createSourceStateManager = function (): SourceStateManager {
+    return new MockSourceStateManager()
+}
