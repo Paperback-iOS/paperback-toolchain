a7c6d03a12e462956d8e3f75aef8f6caa6fd6891
Chance Zibolski <chance.zibolski@gmail.com>
2024-01-22 11:26:58 -0800

    Fix generate-ts.sh
    `npx` doesn't use the PWD but rather it uses the directory package.json
    is located in, so we need to switch to using absolute paths.
    
    Signed-off-by: Chance Zibolski <chance.zibolski@gmail.com>

--- packages/toolchain/src/devtools/generate-ts.sh
+++ packages/toolchain/src/devtools/generate-ts.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-cd "$(dirname "$0")"
 
-rm -rf generated/typescript
-mkdir -p generated/typescript
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# get the absolute path to the package dir
+PACKAGE_DIR="$(realpath "${SCRIPT_DIR}/../..")"
+DEVTOOLS_DIR="${PACKAGE_DIR}/src/devtools"
+
+rm -rf "$DEVTOOLS_DIR/generated/typescript"
+mkdir -p "$DEVTOOLS_DIR/generated/typescript"
+
+PROTO_FILES=()
+while IFS='' read -r line; do
+  PROTO_FILES+=("$line")
+done < <(find "${DEVTOOLS_DIR}/protobuf" -type f -name '*.proto')
+
+echo "${PROTO_FILES[@]}"
 
 echo "Generating TS Files"
-npx protoc \
---ts_out ./generated/typescript \
---ts_opt client_grpc1 \
---proto_path ./protobuf protobuf/*.proto
+npx \
+  protoc \
+  --ts_out "${DEVTOOLS_DIR}/generated/typescript" \
+  --ts_opt client_grpc1 \
+  --proto_path "${DEVTOOLS_DIR}/protobuf" \
+  "${PROTO_FILES[@]}"
 
 # npx grpc_tools_node_protoc \
 # --js_out=import_style=commonjs,binary:./generated/typescript \
