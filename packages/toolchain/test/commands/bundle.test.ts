788cb29f1b4e725fd4bdaacfe9c02398536e2396
Faizan Durrani <faizandurrani324@gmail.com>
2026-03-12 22:10:40 +0500

    updated packages, resolved audit issues

--- packages/toolchain/test/commands/bundle.test.ts
+++
@@ -1,9 +0,0 @@
-import { runCommand } from '@oclif/test'
-import { expect } from 'chai'
-
-describe('bundle', () => {
-  it('runs bundle', async () => {
-    const { stdout } = await runCommand('bundle')
-    expect(stdout).to.contain('Working directory:')
-  })
-})
--- packages/toolchain/test/commands/logcat.test.ts
+++
@@ -1,9 +0,0 @@
-import { runCommand } from '@oclif/test'
-import { expect } from 'chai'
-
-describe('logcat', () => {
-  it('runs logcat with no parameters', async () => {
-    const { stdout } = await runCommand('logcat')
-    expect(stdout).to.contain('')
-  })
-})
